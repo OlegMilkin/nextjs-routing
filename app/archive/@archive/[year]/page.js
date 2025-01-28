3f0a7d2b69f9c4e4ccb0ed402e9114a55319f681
olegMilkin <olegek.milkin@gmail.com>
2025-01-28 23:01:17 +0100

    routing

--- app/archive/@archive/[year]/page.js
+++
@@ -1,9 +0,0 @@
-import NewsList from "@/app/components/news-list";
-import { getNewsForYear } from "@/app/lib/news";
-
-export default function filteredNewsPage ({params}) {
-    const newsYear = params.year;
-    const news = getNewsForYear(newsYear);
-
-    return <NewsList news={news} />
-}
